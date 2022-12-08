ea6b4b9f47992f4db5699a7d48d3d5ef16b35e9f
edwinmahendra <edwin.mahendra@ti.ukdw.ac.id>
2022-12-08 21:37:10 +0700

    add login and register activity

---
+++ app/src/main/java/com/dicoding/picodiploma/carewithus/loginactivity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.picodiploma.carewithus.loginactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.picodiploma.carewithus.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/dicoding/picodiploma/carewithus/registeractivity/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.picodiploma.carewithus.registeractivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.picodiploma.carewithus.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
