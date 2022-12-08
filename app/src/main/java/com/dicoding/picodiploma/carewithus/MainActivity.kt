c4749025ac2d5105d2bb797a63f20fcd2036b314
noelperkasaa <immanuel.joy@ti.ukdw.ac.id>
2022-12-08 22:43:10 +0700

    Update Title Action Bar
    Dari Edwin Home ke Noel Home

--- app/src/main/java/com/dicoding/picodiploma/carewithus/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/carewithus/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportActionBar?.title = "Edwin Home"
+        supportActionBar?.title = "Noel Home"
     }
 }
