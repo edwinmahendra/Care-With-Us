850e133fe2c4fddbb8f4fff5de0e3a4ce634d3e6
alberaguma69 <108799379+alberaguma69@users.noreply.github.com>
2022-12-08 23:15:14 +0700

    Update MainActivity.kt
    Ubah nama

--- app/src/main/java/com/dicoding/picodiploma/carewithus/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/carewithus/MainActivity.kt
@@ -8,7 +8,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportActionBar?.title = "Edwin Home"
-        tes
+        supportActionBar?.title = "Agung Home"
+
     }
 }
