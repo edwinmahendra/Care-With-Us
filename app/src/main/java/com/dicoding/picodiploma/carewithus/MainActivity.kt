de1941a728bab71a0eab7a8df320e96f89d93ab9
edwinmahendra <edwin.mahendra@ti.ukdw.ac.id>
2022-12-08 20:42:12 +0700

    tambahkan judul

--- app/src/main/java/com/dicoding/picodiploma/carewithus/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/carewithus/MainActivity.kt
@@ -7,7 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = "Edwin Home"
     }
+
 
 
 }
