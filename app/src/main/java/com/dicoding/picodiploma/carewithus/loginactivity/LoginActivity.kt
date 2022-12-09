93c6750d41a21ded8649a50f00bdebbc2478fb70
edwinmahendra <edwin.mahendra@ti.ukdw.ac.id>
2022-12-09 09:25:34 +0700

    add intent in login and register activity

--- app/src/main/java/com/dicoding/picodiploma/carewithus/loginactivity/LoginActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/carewithus/loginactivity/LoginActivity.kt
@@ -1,24 +1,40 @@
 package com.dicoding.picodiploma.carewithus.loginactivity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Bundle
-import com.dicoding.picodiploma.carewithus.R
+import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.picodiploma.carewithus.customview.EmailCustomView
 import com.dicoding.picodiploma.carewithus.databinding.ActivityLoginBinding
+import com.dicoding.picodiploma.carewithus.registeractivity.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
     private lateinit var emailCustomView: EmailCustomView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
 
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
 
         emailCustomView = binding.emailLogin
+
+        dhaveAccount()
+    }
+
+    private fun dhaveAccount() {
+        binding.buttonLogin.setOnClickListener {
+            val intent = Intent(this@LoginActivity, RegisterActivity::class.java)
+            val options = ActivityOptions.makeCustomAnimation(
+                this,
+                android.R.anim.fade_in,
+                android.R.anim.fade_out
+            )
+            this.startActivity(intent, options.toBundle())
+        }
     }
 
 
--- app/src/main/java/com/dicoding/picodiploma/carewithus/registeractivity/RegisterActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/carewithus/registeractivity/RegisterActivity.kt
@@ -1,12 +1,33 @@
 package com.dicoding.picodiploma.carewithus.registeractivity
 
+import android.app.ActivityOptions
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dicoding.picodiploma.carewithus.R
+import com.dicoding.picodiploma.carewithus.databinding.ActivityLoginBinding
+import com.dicoding.picodiploma.carewithus.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        haveAccount()
+    }
+
+    private fun haveAccount() {
+        binding.buttonRegister.setOnClickListener{
+            val intent = Intent(this@RegisterActivity, RegisterActivity::class.java)
+            val options = ActivityOptions.makeCustomAnimation(
+                this,
+                android.R.anim.fade_in,
+                android.R.anim.fade_out
+            )
+            this.startActivity(intent, options.toBundle())
+        }
     }
 }
