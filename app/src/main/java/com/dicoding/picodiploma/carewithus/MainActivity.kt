726a7b8b0d38a34472536286f6ef4adfe8200553
edwinmahendra <edwin.mahendra@ti.ukdw.ac.id>
2022-12-09 14:12:15 +0700

    Update MainActivity.kt

--- app/src/main/java/com/dicoding/picodiploma/carewithus/MainActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/carewithus/MainActivity.kt
@@ -38,6 +38,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
+        val intent = Intent(Intent.ACTION_MAIN)
+        intent.addCategory(Intent.CATEGORY_HOME)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        startActivity(intent)
         finish()
     }
 }
