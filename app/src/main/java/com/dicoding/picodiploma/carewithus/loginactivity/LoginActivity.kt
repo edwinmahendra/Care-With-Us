1d9001d12d96cd403718c69e2e255d484dbe8e1b
edwinmahendra <edwin.mahendra@ti.ukdw.ac.id>
2022-12-09 10:37:03 +0700

    add Button Customview

--- app/src/main/java/com/dicoding/picodiploma/carewithus/loginactivity/LoginActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/carewithus/loginactivity/LoginActivity.kt
@@ -3,8 +3,12 @@ package com.dicoding.picodiploma.carewithus.loginactivity
 import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.picodiploma.carewithus.customview.ButtonCustomView
 import com.dicoding.picodiploma.carewithus.customview.EmailCustomView
+import com.dicoding.picodiploma.carewithus.customview.PasswordCustomView
 import com.dicoding.picodiploma.carewithus.databinding.ActivityLoginBinding
 import com.dicoding.picodiploma.carewithus.registeractivity.RegisterActivity
 
@@ -12,6 +16,8 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
     private lateinit var emailCustomView: EmailCustomView
+    private lateinit var passwordCustomView: PasswordCustomView
+    private lateinit var buttonCustomView: ButtonCustomView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +27,27 @@ class LoginActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         emailCustomView = binding.emailLogin
+        passwordCustomView = binding.passwordLogin
+        buttonCustomView = binding.buttonLogin
+
+        emailCustomView.addTextChangedListener(textChangedListener)
+        passwordCustomView.addTextChangedListener(textChangedListener)
 
         dhaveAccount()
+    }
+
+    private val textChangedListener = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            val resultEmail = emailCustomView.text.toString().trim()
+            val resultPass = passwordCustomView.text.toString().trim()
+            buttonCustomView.isEnabled = resultEmail.isNotEmpty() && resultPass.isNotEmpty()
+        }
+
+        override fun afterTextChanged(s: Editable) {
+        }
     }
 
     private fun dhaveAccount() {
--- app/src/main/java/com/dicoding/picodiploma/carewithus/registeractivity/RegisterActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/carewithus/registeractivity/RegisterActivity.kt
@@ -2,16 +2,22 @@ package com.dicoding.picodiploma.carewithus.registeractivity
 
 import android.app.ActivityOptions
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.dicoding.picodiploma.carewithus.R
-import com.dicoding.picodiploma.carewithus.databinding.ActivityLoginBinding
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.picodiploma.carewithus.customview.ButtonCustomView
+import com.dicoding.picodiploma.carewithus.customview.EmailCustomView
+import com.dicoding.picodiploma.carewithus.customview.PasswordCustomView
 import com.dicoding.picodiploma.carewithus.databinding.ActivityRegisterBinding
 import com.dicoding.picodiploma.carewithus.loginactivity.LoginActivity
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var emailCustomView: EmailCustomView
+    private lateinit var passwordCustomView: PasswordCustomView
+    private lateinit var buttonCustomView: ButtonCustomView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
@@ -20,10 +26,17 @@ class RegisterActivity : AppCompatActivity() {
         haveAccount()
 
         supportActionBar?.hide()
+
+        emailCustomView = binding.inputEmail
+        passwordCustomView = binding.passwordLogin
+        buttonCustomView = binding.buttonRegister
+
+        emailCustomView.addTextChangedListener(textChangedListener)
+        passwordCustomView.addTextChangedListener(textChangedListener)
     }
 
     private fun haveAccount() {
-        binding.buttonLogin.setOnClickListener{
+        binding.buttonLogin.setOnClickListener {
             val intent = Intent(this@RegisterActivity, LoginActivity::class.java)
             val options = ActivityOptions.makeCustomAnimation(
                 this,
@@ -31,6 +44,20 @@ class RegisterActivity : AppCompatActivity() {
                 android.R.anim.fade_out
             )
             this.startActivity(intent, options.toBundle())
+        }
+    }
+
+    private val textChangedListener = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+            val resultEmail = emailCustomView.text.toString().trim()
+            val resultPass = passwordCustomView.text.toString().trim()
+            buttonCustomView.isEnabled = resultEmail.isNotEmpty() && resultPass.isNotEmpty()
+        }
+
+        override fun afterTextChanged(s: Editable) {
         }
     }
 }
