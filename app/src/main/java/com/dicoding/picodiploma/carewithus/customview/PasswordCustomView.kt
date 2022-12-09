ad3cfc50c612ef22fa44fc01d40eb2aab4a146d0
edwinmahendra <edwin.mahendra@ti.ukdw.ac.id>
2022-12-09 08:36:50 +0700

    fix ui for loginactivity

--- app/src/main/java/com/dicoding/picodiploma/carewithus/customview/PasswordCustomView.kt
+++ app/src/main/java/com/dicoding/picodiploma/carewithus/customview/PasswordCustomView.kt
@@ -1,4 +1,72 @@
 package com.dicoding.picodiploma.carewithus.customview
 
-class PasswordCustomView {
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.text.method.PasswordTransformationMethod
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import com.dicoding.picodiploma.carewithus.R
+
+class PasswordCustomView: AppCompatEditText{
+    private lateinit var passwordIconDrawable: Drawable
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        transformationMethod = PasswordTransformationMethod.getInstance()
+    }
+
+    private fun init() {
+        passwordIconDrawable =
+            ContextCompat.getDrawable(context, R.drawable.ic_baseline_lock_24) as Drawable
+        inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD
+        compoundDrawablePadding = 16
+
+        setHint(R.string.input_password)
+        setAutofillHints(AUTOFILL_HINT_PASSWORD)
+        setDrawable(passwordIconDrawable)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun afterTextChanged(p0: Editable?) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (!s.isNullOrEmpty() && s.length < 6)
+                    error = context.getString(R.string.password_not_valid, PASSWORD_LEN)
+            }
+        })
+    }
+
+    private fun setDrawable(
+        start: Drawable? = null,
+        top: Drawable? = null,
+        end: Drawable? = null,
+        bottom: Drawable? = null
+    ) {
+        setCompoundDrawablesWithIntrinsicBounds(start, top, end, bottom)
+    }
+
+    companion object {
+        private const val PASSWORD_LEN = 6
+    }
 }
