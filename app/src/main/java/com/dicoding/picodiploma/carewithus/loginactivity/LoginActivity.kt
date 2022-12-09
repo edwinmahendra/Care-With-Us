a704354af7f7577f7a7c524836cbadaa2fdb543e
edwinmahendra <edwin.mahendra@ti.ukdw.ac.id>
2022-12-09 08:18:50 +0700

    fixing custview

--- app/src/main/java/com/dicoding/picodiploma/carewithus/loginactivity/LoginActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/carewithus/loginactivity/LoginActivity.kt
@@ -3,19 +3,22 @@ package com.dicoding.picodiploma.carewithus.loginactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dicoding.picodiploma.carewithus.R
+import com.dicoding.picodiploma.carewithus.customview.EmailCustomView
 import com.dicoding.picodiploma.carewithus.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-
+    private lateinit var emailCustomView: EmailCustomView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
 
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.hide()
 
-
+        emailCustomView = binding.emailLogin
     }
 
 
