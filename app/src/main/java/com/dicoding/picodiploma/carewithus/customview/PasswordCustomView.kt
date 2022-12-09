8ad1860fc0695c367cb8a43252090422ce234d95
edwinmahendra <edwin.mahendra@ti.ukdw.ac.id>
2022-12-09 08:03:48 +0700

    customview login added

---
+++ app/src/main/java/com/dicoding/picodiploma/carewithus/customview/PasswordCustomView.kt
@@ -0,0 +1,4 @@
+package com.dicoding.picodiploma.carewithus.customview
+
+class PasswordCustomView {
+}
--- app/src/main/java/com/dicoding/picodiploma/carewithus/loginactivity/LoginActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/carewithus/loginactivity/LoginActivity.kt
@@ -3,13 +3,19 @@ package com.dicoding.picodiploma.carewithus.loginactivity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.dicoding.picodiploma.carewithus.R
+import com.dicoding.picodiploma.carewithus.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        supportActionBar?.title = "Login Page"
+        supportActionBar?.hide()
+
+
     }
 
 
