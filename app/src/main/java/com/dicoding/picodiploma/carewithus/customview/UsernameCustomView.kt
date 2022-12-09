dd3e90941aaf00b0d55d1d84a5666661326ad879
edwinmahendra <edwin.mahendra@ti.ukdw.ac.id>
2022-12-09 09:19:55 +0700

    Register Activity with UI updated

---
+++ app/src/main/java/com/dicoding/picodiploma/carewithus/customview/UsernameCustomView.kt
@@ -0,0 +1,145 @@
+package com.dicoding.picodiploma.carewithus.customview
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.InputType
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat
+import com.dicoding.picodiploma.carewithus.R
+
+class UsernameCustomView: AppCompatEditText, View.OnTouchListener {
+    private lateinit var clearButtonImage: Drawable
+    private lateinit var userIconDrawable: Drawable
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    private fun init() {
+        userIconDrawable =
+            ContextCompat.getDrawable(context, R.drawable.ic_baseline_person_24) as Drawable
+        inputType = InputType.TYPE_CLASS_TEXT
+        compoundDrawablePadding = 16
+        setHint(R.string.input_name)
+        setDrawable(userIconDrawable)
+        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.ic_baseline_close_24) as Drawable
+        setOnTouchListener(this)
+
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if (s.toString().isNotEmpty()) {
+                    showClearButton()
+                } else {
+                    hideClearButton()
+                }
+            }
+
+            override fun afterTextChanged(s: Editable) {
+            }
+        })
+    }
+
+    override fun onTouch(v: View?, event: MotionEvent): Boolean {
+
+        if (compoundDrawables[2] != null) {
+            val clearButtonStart: Float
+            val clearButtonEnd: Float
+            var isClearButtonClicked = false
+            if (layoutDirection == View.LAYOUT_DIRECTION_RTL) {
+                clearButtonEnd = (clearButtonImage.intrinsicWidth + paddingStart).toFloat()
+                when {
+                    event.x < clearButtonEnd -> isClearButtonClicked = true
+                }
+            } else {
+                clearButtonStart = (width - paddingEnd - clearButtonImage.intrinsicWidth).toFloat()
+                when {
+                    event.x > clearButtonStart -> isClearButtonClicked = true
+                }
+            }
+            if (isClearButtonClicked) {
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        clearButtonImage =
+                            ContextCompat.getDrawable(context, R.drawable.ic_baseline_close_24) as Drawable
+                        showClearButton()
+                        return true
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        clearButtonImage =
+                            ContextCompat.getDrawable(context, R.drawable.ic_baseline_close_24) as Drawable
+                        when {
+                            text != null -> text?.clear()
+                        }
+                        hideClearButton()
+                        showIconPeople()
+                        return true
+                    }
+
+                    else -> return false
+                }
+            } else {
+                showIconPeople()
+                return false
+            }
+        }
+        showIconPeople()
+        return false
+    }
+
+    private fun showClearButton() {
+        setButtonDrawables(endOfTheText = clearButtonImage)
+    }
+
+    private fun hideClearButton() {
+        setButtonDrawables()
+    }
+
+    private fun showIconPeople() {
+        setDrawable(start = userIconDrawable)
+    }
+
+    private fun setButtonDrawables(
+        startOfTheText: Drawable? = null,
+        topOfTheText: Drawable? = null,
+        endOfTheText: Drawable? = null,
+        bottomOfTheText: Drawable? = null
+    ) {
+        setCompoundDrawablesWithIntrinsicBounds(
+            startOfTheText,
+            topOfTheText,
+            endOfTheText,
+            bottomOfTheText
+        )
+    }
+
+    private fun setDrawable(
+        start: Drawable? = null,
+        top: Drawable? = null,
+        end: Drawable? = null,
+        bottom: Drawable? = null
+    ) {
+        setCompoundDrawablesWithIntrinsicBounds(start, top, end, bottom)
+    }
+
+}
